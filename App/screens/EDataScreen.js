0e387e6590b6163e154ccb89c9e203aa0328f725
Samir Ali <353385354@educ.dpcdsb.org>
2022-01-12 17:03:12 -0500

    quick changes

--- App/screens/EDataScreen.js
+++ App/screens/EDataScreen.js
@@ -1,127 +1,167 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableHighlight, ScrollView } from 'react-native';
-import * as Application from 'expo-application';
-import api from '../../src/calls.js'
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import { FloatingLabelInput } from 'react-native-floating-label-input';
-import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
-import { Appbar } from 'react-native-paper'
+  import React, { useState } from 'react';
+  import { View, StyleSheet, TouchableHighlight, ScrollView } from 'react-native';
+  import * as Application from 'expo-application';
+  import api from '../../src/calls.js'
+  import { useForm, Controller} from "react-hook-form"
+  import { Appbar } from 'react-native-paper'
 
-const EDataScreen = ({ navigation }) => {
-
-  const deviceId = Application.androidId
-  const [name, setName] = useState('')
-  const [age, setAge] = useState('')
-  const [phoneNumber, setphoneNumber] = useState('')
-  const [numberOfDates, setnumberOfDates] = useState('')
-  const [lastDate, setlastDate] = useState('')
-  const [notes, setNotes] = useState('')
+  const EDataScreen = ({ navigation }) => {
+    const { control, handleSubmit, errors } = useForm();
+    const deviceId = Application.androidId
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+    const [phoneNumber, setphoneNumber] = useState('')
+    const [numberOfDates, setnumberOfDates] = useState('')
+    const [lastDate, setlastDate] = useState('')
+    const [notes, setNotes] = useState('')
 
 
-  const onPress = async (e) => {
-    e.preventDefault()
+    const onPress = async (e) => {
+      e.preventDefault()
 
-    const newDate = {
-      name,
-      age: parseInt(age),
-      phoneNumber,
-      numberOfDates: parseInt(numberOfDates),
-      lastDate,
-      // image: e.target.elements.image.value,
-      notes
+      const newDate = {
+        name,
+        age: parseInt(age),
+        phoneNumber,
+        numberOfDates: parseInt(numberOfDates),
+        lastDate,
+        // image: e.target.elements.image.value,
+        notes
+      }
+
+      api.addDate(deviceId, newDate)
+        .then((result) => {
+          alert('Date was added')
+          navigation.navigate('MainScreen')
+        })
+        .catch((e) => {
+          alert('Error', e)
+        })
     }
 
-    api.addDate(deviceId, newDate)
-      .then((result) => {
-        alert('Date was added')
-        navigation.navigate('MainScreen')
-      })
-      .catch((e) => {
-        alert('Error', e)
-      })
-  }
+    return (
+      <View style={styles.container}>
+          <View style={styles.top}></View>
 
-  return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{
-        flex: 1,
-        justifyContent: 'space-around',
-      }}
-    >
-      <Appbar.Header style={{ backgroundColor: '#FF7A93' }}>
-        <Appbar.Content title="Enter your new date" titleStyle={{ textAlign: "center", fontSize: 25 }} />
-        <Appbar.BackAction style={{position:"absolute"}}onPress={navigation.goBack} />
-      </Appbar.Header>
-      <FloatingLabelInput
-        label="Name"
-        onChangeText={value => setName(value)}
-        value={name}
-        required={true}
-      />
-      <FloatingLabelInput
-        label="Age"
-        keyboardType="numeric"
-        onChangeText={value => setAge(value)}
-        value={age}
-      />
-      <FloatingLabelInput
-        label="Phone Number"
-        mask="(999)-999-9999"
-        keyboardType="numeric"
-        onChangeText={value => setphoneNumber(value)}
-        value={phoneNumber}
-      />
-      <FloatingLabelInput
-        label="Number of Dates"
-        keyboardType="numeric"
-        onChangeText={value => setnumberOfDates(value)}
-        value={numberOfDates}
-      />
-      <FloatingLabelInput
-        label="Last Date (MM/DD/YY)"
-        mask="99/99/9999"
-        keyboardType='numeric'
-        onChangeText={value => setlastDate(value)}
-        value={lastDate}
-      />
-      <FloatingLabelInput
-        label="Additional Notes"
-        onChangeText={value => setNotes(value)}
-        value={notes}
-      />
-      <View>
-        <TouchableHighlight
-          onPress={onPress}
-          style={styles.button}
-        >
-          <Text> Done </Text>
-        </TouchableHighlight>
+          <View style={styles.middle}>
+            <Text style={styles.textContainer}>You are ready to go</Text>
 
-      </View>
-    </ScrollView>
-  );
-};
+            <View style={styles.formArea}>
+              <Text style={[styles.textContainer, styles.signin]}>Sign in</Text>
+              <Form style={styles.mainForm}>
+                <Item style={styles.formItems}>
+                  <Input placeholder="Username" style={styles.Input} />
+                </Item>
+                <Item style={styles.formItems}>
+                  <Input placeholder="Password" style={styles.Input} />
+                </Item>
+                <View style={styles.loginAs}>
+                  <Text style={styles.loginText}>Login as</Text>
+                  <Body>
+                    <Text style={styles.cboxText}>Admin</Text>
+                  </Body>
+                  <Body>
+                    <Text style={styles.cboxText}>User</Text>
+                  </Body>
+                </View>
+                <View style={styles.Button}>
+                  <Button block style={styles.mainBtn}>
+                    <Text style={styles.btnText}>Submit</Text>
+                  </Button>
+                </View>
+              </Form>
+            </View>
+          </View>
+          <View style={styles.bottom}></View>
+        </View>
+      );
+  };
 
-const styles = StyleSheet.create({
-  button: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 24,
-    paddingVertical: 8,
-    backgroundColor: '#3498db',
-    borderRadius: 8,
-    shadowColor: '#2a2a2a',
-    shadowOpacity: 0.15,
-    shadowRadius: 4,
-    shadowOffset: {
-      width: 0,
-      height: 2
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      position: 'relative',
     },
-    elevation: 2
-  }
-});
+    top: {
+      position: 'relative',
+      backgroundColor: '#5257F2',
+      paddingRight: 12.7,
+      paddingLeft: 12.7,
+      height: 250,
+    },
+    middle: {
+      width: '100%',
+      height: '100%',
+      flex: 1,
+      position: 'absolute',
+      zIndex: 2,
+      backgroundColor: 'transparent',
+      paddingLeft: 26.3,
+      paddingRight: 26.3,
+    },
+    bottom: {
+      position: 'relative',
+      height: '100%',
+      paddingRight: 12.7,
+      paddingLeft: 12.7,
+      backgroundColor: '#5257F2',
+    },
+    textContainer: {
+      color: '#FCFDFF',
+      fontSize: 24,
+      marginBottom: 30,
+      position: 'relative',
+      top: '20%',
+      alignSelf: 'center',
+    },
+    formArea: {
+      alignSelf: 'center',
+      width: '100%',
+      backgroundColor: '#ffffff',
+      borderRadius: 5,
+      top: '20%',
+      paddingBottom: 40,
+    },
+    signin: {
+      top: 0,
+      color: '#2D3057',
+      marginTop: 15,
+    },
+    formItems: {
+      marginTop: 15,
+      borderBottomColor: '#2D3057',
+    },
+    Input: {
+      fontFamily: 'Poppins-Bold',
+      fontSize: 12,
+    },
+    loginAs: {
+      paddingLeft: 46.6,
+      display: 'flex',
+      flexDirection: 'row',
+      marginTop: 15,
+      marginBottom: 20,
+      alignItems: 'center',
+    },
+    loginText: {
+      color: '#2D3057',
+      fontSize: 10,
+      fontWeight: 'bold',
+    },
+    cboxText: {
+      fontSize: 10,
+    },
+    Button: {
+      padding: 30.8,
+      borderRadius: 4,
+    },
+    mainBtn: {
+      backgroundColor: '#1DDCAF',
+    },
+    btnText: {
+      color: '#2D3057',
+      fontSize: 12,
+    },
+  });
 
-export default EDataScreen;
+  export default EDataScreen;
